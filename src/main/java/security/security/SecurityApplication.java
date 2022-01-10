8e3767baa742f13b2d2d7ba5575c51fe7fbde057
taylorhelene <taylorhelene09@gmail.com>
2022-01-10 20:30:56 +0300

    y

--- src/main/java/security/security/SecurityApplication.java
+++ src/main/java/security/security/SecurityApplication.java
@@ -3,7 +3,6 @@ package security.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SecurityApplication {
 
