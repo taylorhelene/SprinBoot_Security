489d7646fc0a2303cd3337be13d81259cbea2906
taylorhelene <taylorhelene09@gmail.com>
2022-01-10 20:19:10 +0300

    first commit

---
+++ src/main/java/security/security/HomeController.java
@@ -0,0 +1,41 @@
+package security.security;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+	public String getHomePage() {
+		return "homePage";
+	}
+	
+	@GetMapping("/welcome")
+	public String getWelcomePage() {
+		return "welcomePage";
+	}
+	
+	@GetMapping("/admin")
+	public String getAdminPage() {
+		return "adminPage";
+	}
+    
+	@GetMapping("/emp")
+	public String getEmployeePage() {
+		return "empPage";
+	}
+	
+	@GetMapping("/mgr")
+	public String getManagerPage() {
+		return "mgrPage";
+	}
+	
+	@GetMapping("/common")
+	public String getCommonPage() {
+		return "commonPage";
+	}
+    
+	@GetMapping("/accessDenied")
+	public String getAccessDeniedPage() {
+		return "accessDeniedPage";
+	}
+}
